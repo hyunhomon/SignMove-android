13a2a57f9d1327bcf1d4932610970e4543d029a9
hyunhomon <hyunhomon@gmail.com>
2023-12-14 03:44:28 +0900

    init

---
+++ app/src/main/java/com/example/signmove/Application.kt
@@ -0,0 +1,13 @@
+package com.example.signmove
+
+import android.app.Application
+
+class Application : Application() {
+    companion object {
+        //
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
